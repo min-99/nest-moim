0038521cf434f8ebab350af9fa6b10e039d18669
insterior-ward <ward@insterior.co.kr>
2022-01-26 14:30:03 +0900

    docker 환경 수정
    host directory 참조하도록 변경

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN npm run build
 FROM node:14.17.0 AS runner
 WORKDIR /app
 COPY --from=builder /app ./
-CMD ["npm", "run", "start:prod"]
+CMD ["npm", "run", "start:dev"]
--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+docker kill nest-moim-container
 docker rm nest-moim-container
-docker run --name nest-moim-container -p 3001:3001 -d nest-moim
+docker run --name nest-moim-container -v $PWD:/app -p 3001:3001 -d nest-moim
